f276e286684853e6acca219df2fca59ed2c9b437
Pietro Belloni <pietro.belloni@outlook.it>
2023-08-22 12:00:18 +0200

    Update MARMoT documentation

--- R/MARMoT.R
+++ R/MARMoT.R
@@ -1,22 +1,53 @@
 #' MARMoT balancing method
 #'
 #' @description
-#' Matching on poset-based average rank for multiple treatments (MARMoT)
+#' Matching on poset-based average rank for multiple treatments (MARMoT).
+#' 
+#' @details
+#' There are many scenarios where classic propensity score techniques are not
+#' applicable (e.g. there are many treatments). In a multiple-treatment
+#' framework, MARMoT is a method to balance the distribution of covariates among
+#' several treatment groups. MARMoT introduces a method for achieving balance
+#' among treatment groups by utilizing partially ordered set (poset) theory.
+#' This approach focuses on equalizing individual characteristics without
+#' relying on propensity score techniques or a dependent variable. Unlike
+#' propensity score methods, poset theory doesn't require assumptions about
+#' model specifications for treatment allocation. Each subject is represented by
+#' a profile of their characteristics, and an average rank approximation is
+#' associated with each profile. This value represents the significance of
+#' individual characteristics for treatment allocation and can be normalized for
+#' better interpretability.                                                         2021b).
 #'
 #' @param data
-#' a dataframe or equivalent
+#' A dataframe or equivalent.
 #' @param confounders
-#' a vector containing the column names of the confounders to balance by
+#' A vector containing the column names of the confounders to balance by.
 #' @param treatment
-#' a string indicating the column name of the treatment variable
+#' A string indicating the column name of the treatment variable.
 #' @param reference
-#' "median" default, the statistic used to determine the reference frequencies in the balancing process
+#' The statistic used to determine the reference frequencies in the balancing
+#' process. Default is median.
 #' @param n.cores
-#' 1 default, number of cores to be used (Linux and Mac systems only!); if a number grater than 1 is specified the function will use a parallelized version of the deloof approximation
+#' Number of cores to be used (Linux and Mac systems only!); if a
+#' number grater than 1 is specified the function will use a parallelized
+#' version of the deloof approximation. Default set to 1.
 #'
 #' @return
-#' a list of objects, also containing the balanced dataset with the same structure of the input dataset
+#' A list of objects, also containing the balanced dataset with the same
+#' structure of the input dataset.
 #' @export
+#'
+#' @references
+#' Silan, Margherita, Arpino, Bruno and Boccuzzo, Giovanna (2021), "Evaluating
+#' inverse propensity score weighting in the presence of many treatments. An
+#' application to the estimation of the neighbourhood efect.", Journal of
+#' Statistical Computation and Simulation, 91(4), 836–859.
+#' https://doi.org/10.1080/00949655.2020.1832092
+#' 
+#' Silan, Margherita, Arpino, Bruno and Boccuzzo, Giovanna (2021), "Matching
+#' on poset-based average rank for multiple treatments to compare many
+#' unbalanced groups.", Statistics in Medocone, 40(28), 6443–6458.
+#' https://doi.org/10.1002/sim.9192
 #'
 #' @examples
 #' out = MARMoT(data = MARMoT_data, confounders = c("race", "age"),
